eb66160beec47f8123ba41d6e6f34550f75a33cf
Asko Soukka <asko.soukka@iki.fi>
2017-08-21 23:21:29 +0300

    Preparing release 0.9.4

--- CHANGES.md
+++ CHANGES.md
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-0.9.4 (unreleased)
+0.9.4 (2017-08-21)
 ------------------
 
 - Add support for serializing plone.app.textfield RichTextValues
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.9.4.dev0'
+version = '0.9.4'
 
 long_description = (
     open('README.md').read() + '\n' +
